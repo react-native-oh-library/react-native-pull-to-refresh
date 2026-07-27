8854608d7828a6e48ce547dad73cb36579708f6f
tyBrave <103195921+tyBrave@users.noreply.github.com>
2026-07-27 15:05:18 +0800

    fix:修复没有配置footer的情况下，下拉刷新正常刷新 (#38)
    Signed-off-by: tyBrave <tianyong21@h-partners.com>

--- harmony/pull_to_refresh/src/main/cpp/RNCPullToRefreshInstance.cpp
+++ harmony/pull_to_refresh/src/main/cpp/RNCPullToRefreshInstance.cpp
@@ -210,6 +210,9 @@ void RNCPullToRefreshInstance::panGesture(ArkUI_NodeHandle arkUI_NodeHandle) {
 }
 
 void RNCPullToRefreshInstance::onActionPullUpdate() {
+    if (!m_headerInstance) {
+        return;
+    }
     if (state == FREE || state == PULL_DOWN_1 || state == PULL_DOWN_2 || state == PULL_UP) {
         touchYNew = offsetY;
         if (!isComponentTop()) {
@@ -234,6 +237,9 @@ void RNCPullToRefreshInstance::onActionPullUpdate() {
 }
 
 void RNCPullToRefreshInstance::onActionPullEnd() {
+    if (!m_headerInstance) {
+        return;
+    }
     auto maxTranslate = MAX_TRANSLATE;
     if (trYTop > 0 && up_status == Up_FREE) {
         if (state == FREE || state == PULL_DOWN_1 || state == PULL_DOWN_2) {
@@ -250,6 +256,9 @@ void RNCPullToRefreshInstance::onActionPullEnd() {
     }
 }
 void RNCPullToRefreshInstance::onActionUpEnd() {
+    if (!m_footerInstance) {
+        return;
+    }
     auto maxTranslate = MAX_TRANSLATE;
     if (trYTop > 0 && state == FREE) {
         if (up_status == Up_FREE || up_status == Up_PULL_DOWN_1 || up_status == Up_PULL_DOWN_2) {
@@ -273,6 +282,9 @@ void RNCPullToRefreshInstance::onActionUpEnd() {
 }
 
 void RNCPullToRefreshInstance::onActionUpUpdate() {
+    if (!m_footerInstance) {
+        return;
+    }
     if (up_status == Up_FREE || up_status == Up_PULL_DOWN_1 || up_status == Up_PULL_DOWN_2 || up_status == Up_PULL_UP) {
         touchYNew = offsetY;
         if (!isComponentBottom()) {
